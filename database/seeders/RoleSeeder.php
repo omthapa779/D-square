d9bc22a5262fee2489e262a97e1dfb52f12703c2
Sas4aZ <paudelsashwat16@gmail.com>
2023-03-06 15:42:14 +0545

    database migration with seeds

---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Roles;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Roles::create(['name'=>"Administrator"]);
+        Roles::create(['name'=>"Staff"]);
+        Roles::create(['name'=>"User"]);
+    }
+}
