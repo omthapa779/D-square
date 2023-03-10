1637ebccd50ad19fecc8b2742a28c94a6cb68543
वीज़ी || Weezi <98094485+Ouiiizi@users.noreply.github.com>
2023-03-10 08:54:58 +0545

    gang

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Mail\ContactMail;
+use App\Models\Student;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Mail;
+use Intervention\Image\Facades\Image;
+
+
+class PagesController extends Controller
+{
+
+
+}
+
+
+
---
+++ resources/views/adminhome.blade.php
@@ -0,0 +1,12 @@
+<html><head><title>Admin Home</title></head>
+<body>
+{{--navbar here --}}
+
+<h1 class="contact_home"><a href="{{url('/jobadd/')}}">ADD A JOB TO THE DASHBOARD HERE</a></h1>
+<
+
+
+
+
+</body>
+</html>
