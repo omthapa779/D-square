572655be54a886d56cba75186b1913ecc85f4933
वीज़ी || Weezi <98094485+Ouiiizi@users.noreply.github.com>
2023-03-16 21:01:05 +0545

    Update adminhome.blade.php

--- resources/views/adminhome.blade.php
+++ resources/views/adminhome.blade.php
@@ -4,8 +4,10 @@
 
 <h1 class="contact_home"><a href="{{url('/jobadd/')}}">ADD A JOB TO THE DASHBOARD HERE</a></h1>
 
+<br>
+<h1 class="announce_home"><a href="{{url('/announce')}}">Want to Announce? Announce Here.</a> </h1>
 
 
-
+{{--add what here--}}
 </body>
 </html>
